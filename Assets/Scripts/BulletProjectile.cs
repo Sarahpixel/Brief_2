8541ef30b48d4bdc65a95804bf5dbaef9141ae9b
sarah nguyen <107088382+Sarahpixel@users.noreply.github.com>
2023-03-28 23:25:15 +1100

    Enemy +Bullet

---
+++ Assets/Scripts/BulletProjectile.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    [SerializeField] private Transform vfxExplosion;
+    [SerializeField] private Transform vfxblood;
+    private Rigidbody bulletRigidBody;
+
+
+    private void Awake()
+    {
+        bulletRigidBody = GetComponent<Rigidbody>();
+
+
+    }
+    private void Start()
+    {
+        float speed = 10f;
+        bulletRigidBody.velocity = transform.forward * speed;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<Enemy>() != null)
+        {
+            //hit target
+            other.gameObject.GetComponent<Enemy>().TakeDamage(20);
+            Instantiate(vfxblood, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            //hit something else
+            Instantiate(vfxExplosion, transform.position, Quaternion.identity);
+        }
+        Destroy(gameObject);
+    }
+
+    public float timeRemaining = 5f;
+    private void Update() //deletes the bullet after 5 seconds
+    {
+
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
